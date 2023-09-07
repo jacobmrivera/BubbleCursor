36804894a9d7cbb33e8e491fc173c61976c89db3
Jacob Rivera <jakemichaelrivera32@gmail.com>
2023-09-07 14:18:48 -0500

    Give id's to each circle and add js code to get information about each circle

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 // Function to compute Intersecting Distance
 function intDist(t1, t2) {
-    return p1 * p2;
-  }
+  return p1 * p2;
+}
 
 
-  // Function to computer Containment Distance
-  function conDist(t1, t2) {
-    return
-  }
+// Function to computer Containment Distance
+function conDist(t1, t2) {
+  return
+}
 
-  var cursor = document.getElementById("cursor");
-  document.body.addEventListener("mousemove", function(e) {
-    cursor.style.left = e.clientX + "px",
-      cursor.style.top = e.clientY + "px";
-  });
+function getOrigin(point) {
+  var rect = point.getBoundingClientRect();
+  var centerX = rect.left + rect.width / 2;
+  var centerY = rect.top + rect.height / 2;
+  console.log(`Element ID: ${point.id}`);
+  console.log(`CenterX: ${centerX}px, CenterY: ${centerY}px`);
+}
+
+var cursorRing = document.getElementById("cursor");
+
+document.body.addEventListener("mousemove", function(e) {
+  cursorRing.style.left = e.clientX + "px",
+  cursorRing.style.top = e.clientY + "px";
+});
+
+var circleElements = document.querySelectorAll('.circle');
+
+circleElements.forEach(element => {
+  console.log(element.id); // Access element content or perform other actions
+  getOrigin(element);
+});
+
+
+
+
+
