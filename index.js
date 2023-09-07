7ff58d829ef0378a00d43b6771a039cfc328fed7
Jacob Rivera <jakemichaelrivera32@gmail.com>
2023-09-07 13:54:13 -0500

    Add functionality for a circle around the cursor

---
+++ index.js
@@ -0,0 +1,16 @@
+// Function to compute Intersecting Distance
+function intDist(t1, t2) {
+    return p1 * p2;
+  }
+
+
+  // Function to computer Containment Distance
+  function conDist(t1, t2) {
+    return
+  }
+
+  var cursor = document.getElementById("cursor");
+  document.body.addEventListener("mousemove", function(e) {
+    cursor.style.left = e.clientX + "px",
+      cursor.style.top = e.clientY + "px";
+  });
